2fcab39c918c54cedcb8017df0309dae1a648d05
lukascivil <lukas_civil@hotmail.com>
2020-09-20 17:52:55 -0300

    feat: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
+// Packages
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Tasker Example')
+    .setDescription('The Tasker API description')
+    .setVersion('1.0')
+    .addTag('task')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
 
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
